34c90c4bed290e1cd3ca6d98ecbf70f5affe22fb
alexarriaga2000 <alexarriaga2000@hotmail.com>
2022-08-24 11:23:39 -0500

    Primer desafio complementario

---
+++ js/desafio1.js
@@ -0,0 +1,10 @@
+
+const K = 5
+
+for (let i=0;i<=3;i++){
+
+    let numero = parseInt(prompt("Escribe un numero"))
+    resultado = numero + K
+    alert(resultado )
+
+}
---
+++ js/desafio12.js
@@ -0,0 +1,21 @@
+
+const answerS = "S"
+const answerN = "N"
+valor = false
+
+do{
+    if (valor) {let nombre = prompt("Escribe tu nombre")}
+    let respuesta = prompt("¿Sabes programar en Java (S/N)?")
+    if(respuesta == answerS){
+        alert(nombre + ", estas contratado")
+        valor = false}
+    else if(respuesta == answerN){
+        alert(nombre + ",te invito a que te inscribas al curso en CODERHOUSE")
+        valor = false}
+    else {
+        alert(nombre + ",por favor indica un valor valido")
+        valor = true}
+  }while(valor)
+    //(respuesta !== answerN) || (respuesta == answerS)) 
+    
+alert("Gracias por tu registro")
